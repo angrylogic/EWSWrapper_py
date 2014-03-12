f197725e92db876f0215d41dc4b7acbbde6d1caf
Greg Harris <gharris@angrylogic.net>
2014-03-12 17:36:12 -0400

    Adding WSDL files to distribution

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ from distutils.core import setup
 setup(name="EWSWrapper", version="0.2",
       description="Python wrapper functions for easy use of Microsoft Exchange Web Services",
       author="Michael Korzeniowski", author_email="mko_san@lafiel.net",
-      packages=["EWSWrapper"])
+      packages=["EWSWrapper"], package_data={"EWSWrapper": ["wsdl/*"]})
